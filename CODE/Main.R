2d32846b1562350aa8abefd1b985e43ddee9c0a0
Hcrawford731 <49402032+Hcrawford731@users.noreply.github.com>
2021-03-16 18:42:27 -0400

    tls min and max
    Added TLS min and max

--- CODE/Main.R
+++ CODE/Main.R
@@ -1,33 +1,27 @@
-###############################################################################
-######################### PICK REGION AND UAS DEM TYPE ########################
-uas = minimum
-mask = water.mask
-###############################################################################
-###############################################################################
-
 # filters DEMs by mask
-DEMs = get.DEM(tls, lidar, uas, mask)
+DEMs = get.DEM(tls, lidar, minimum, shadow.mask)
 
 #Make Line Profile
 par(mfrow = c(2, 2))
-profile.tls(DEMs$TLS, DEMs$LiDAR, DEMs$UAS)
-profile.uas(DEMs$TLS, DEMs$LiDAR, DEMs$UAS)
+profile.tls(DEMs$TLS, DEMs$LiDAR, DEMs$UAS, "Shadow")
+profile.uas(DEMs$TLS, DEMs$LiDAR, DEMs$UAS, "Shadow")
 
 # Make error profiles
-error.tls(DEMs$TLS, DEMs$UAS)
-error.uas(DEMs$TLS, DEMs$UAS)
+error.tls(DEMs$TLS, DEMs$UAS, "Shadow")
+error.uas(DEMs$TLS, DEMs$UAS, "Shadow")
+
+
 
 # Examples of how to check differences attributes
-compare.DEM(DEMs$TLS, DEMs$UAS, "TLS and UAS")
-compare.DEM(DEMs$LiDAR, DEMs$UAS, "LiDAR and UAS")
+compare.DEM(DEMs$TLS, DEMs$UAS, "TLS and UAS for low tide")
+compare.DEM(DEMs$LiDAR, DEMs$UAS, "LiDAR and UAS for low tide")
 
 #KS TEST
-mask.two = clearing.mask
-DEMs.two = get.DEM(tls, lidar, uas, mask.two)
-ks.test(as.vector(DEMs$TLS - DEMs$UAS),
-        as.vector(DEMs.two$TLS - DEMs.two$UAS),
-        alternative = "two.sided")
+DEMs.two = get.DEM(tls, lidar, hightide, water.mask)
+#ks.test(as.vector(DEMs$TLS - DEMs$UAS),
+#        as.vector(DEMs.two$TLS - DEMs.two$UAS),
+#        alternative = "two.sided")
 
 #Check attributes in second area
-compare.DEM(DEMs.two$TLS, DEMs.two$UAS, "TLS and UAS")
-compare.DEM(DEMs.two$LiDAR, DEMs.two$UAS, "LiDAR and UAS")
+compare.DEM(DEMs.two$TLS, DEMs.two$UAS, "TLS and UAS for high tide")
+compare.DEM(DEMs.two$LiDAR, DEMs.two$UAS, "LiDAR and UAS for high tide")
